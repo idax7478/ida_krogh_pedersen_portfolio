bb3077947baeaddf475a0f5bc2269bfd088f1d31
ida <idax7478@stud.kea.dk>
2022-09-21 13:52:53 +0200

    ny

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        'mustard': '#B1803E',
+        'sunset': '#CFA78D',
+        'terracotta': '#C68462',
+        'rust': '#7F4C31',
+        'cage': '#9C9A81',
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
