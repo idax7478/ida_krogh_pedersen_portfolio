8cdd2458ac466765a544238fb064d6b7f6857395
ida <idax7478@stud.kea.dk>
2022-09-26 22:36:16 +0200

    ny

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
     extend: {
       color: {
-        'mustard': '#B1803E',
-        'sunset': '#CFA78D',
-        'terracotta': '#C68462',
-        'rust': '#7F4C31',
-        'cage': '#9C9A81',
+        mustard: "#B1803E",
+        sunset: "#CFA78D",
+        terracotta: "#C68462",
+        rust: "#7F4C31",
+        cage: "#9C9A81",
       },
     },
   },
