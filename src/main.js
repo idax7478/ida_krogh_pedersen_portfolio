cf51d7521843704f42304a98bb895ccb351cd722
ida <idax7478@stud.kea.dk>
2022-09-30 19:46:02 +0200

    motion one

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 import "./style.css";
+/* Import af Motion One bibliotek */
+import { animate, stagger, inView, scroll, timeline } from "motion";
+
+animate("body", { opacity: [0, 1] }, { duration: 5 });
